0166153c22ca62e399b57a8d92dff6330556f54e
Vidy2 <111710984+Vidy2@users.noreply.github.com>
2026-04-27 21:25:16 +0000

    Fixed back

--- decompiled/App.java
+++ decompiled/App.java
@@ -2,7 +2,7 @@
  * Decompiled with CFR 0.152.
  */
 import java.util.ArrayList;
-import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class App {
     boolean wash = false;
@@ -58,22 +58,20 @@ public class App {
     }
 
     public void run() {
-        Scanner input = new Scanner(System.in);
-        long start = System.nanoTime();
         while (true) {
-            App.clearConsole();
-            this.displayPresidentsInTwoColumns();
-            this.displayWrongGuesses();
-            System.out.print("\n\nName a president (Type exit to quit): ");
-            String answer = input.nextLine();
-            if (answer.toLowerCase().contains("exit") || answer.toLowerCase().contains("quit")) break;
+            String presidentsList = this.displayPresidentsInTwoColumns();
+            String wrong = this.displayWrongGuesses();
+            String message = presidentsList + "\n\n" + wrong;
+            JOptionPane.showMessageDialog(null, message);
+            String answer = JOptionPane.showInputDialog("Name a president (Type exit to quit):");
+            if (answer == null || answer.toLowerCase().contains("exit") || answer.toLowerCase().contains("quit")) break;
             boolean matched = this.processAnswer(answer.toLowerCase());
             if (matched || this.wrongGuesses.contains(answer)) continue;
             this.wrongGuesses.add(answer);
         }
     }
 
-    private void displayPresidentsInTwoColumns() {
+    private String displayPresidentsInTwoColumns() {
         PresidentEntry[] entries = new PresidentEntry[]{new PresidentEntry("1789-1793", "George Washington", this.wash), new PresidentEntry("1793-1797", "George Washington", this.wash), new PresidentEntry("1797-1801", "John Adams", this.adams), new PresidentEntry("1801-1805", "Thomas Jefferson", this.jefferson), new PresidentEntry("1805-1809", "Thomas Jefferson", this.jefferson), new PresidentEntry("1809-1813", "James Madison", this.madison), new PresidentEntry("1813-1817", "James Madison", this.madison), new PresidentEntry("1817-1821", "James Monroe", this.monroe), new PresidentEntry("1821-1825", "James Monroe", this.monroe), new PresidentEntry("1825-1829", "John Quincy Adams", this.jq_adams), new PresidentEntry("1829-1833", "Andrew Jackson", this.jackson), new PresidentEntry("1833-1837", "Andrew Jackson", this.jackson), new PresidentEntry("1837-1841", "Martin Van Buren", this.buren), new PresidentEntry("1841", "William Henry Harrison", this.w_h_harrison), new PresidentEntry("1841-1845", "John Tyler", this.tyler), new PresidentEntry("1845-1849", "James Polk", this.polk), new PresidentEntry("1849-1850", "Zachary Taylor", this.taylor), new PresidentEntry("1850-1853", "Millard Fillmore", this.fillmore), new PresidentEntry("1853-1857", "Franklin Pierce", this.pierce), new PresidentEntry("1857-1861", "James Buchanan", this.buchanan), new PresidentEntry("1861-1865", "Abraham Lincoln", this.lincoln), new PresidentEntry("1865-1869", "Andrew Johnson", this.johnson), new PresidentEntry("1869-1873", "Ulysses S. Grant", this.grant), new PresidentEntry("1873-1877", "Ulysses S. Grant", this.grant), new PresidentEntry("1877-1881", "Rutherford B. Hayes", this.hayes), new PresidentEntry("1881", "James Garfield", this.garfield), new PresidentEntry("1881-1885", "Chester A. Arthur", this.arthur), new PresidentEntry("1885-1889", "Grover Cleveland", this.cleveland), new PresidentEntry("1889-1893", "Benjamin Harrison", this.b_harrison), new PresidentEntry("1893-1897", "Grover Cleveland", this.cleveland), new PresidentEntry("1897-1901", "William McKinley", this.mckinley), new PresidentEntry("1901-1905", "Theodore Roosevelt", this.t_roosevelt), new PresidentEntry("1905-1909", "Theodore Roosevelt", this.t_roosevelt), new PresidentEntry("1909-1913", "William Taft", this.taft), new PresidentEntry("1913-1917", "Woodrow Wilson", this.wilson), new PresidentEntry("1917-1921", "Woodrow Wilson", this.wilson), new PresidentEntry("1921-1923", "Warren G. Harding", this.harding), new PresidentEntry("1923-1925", "Calvin Coolidge", this.coolidge), new PresidentEntry("1925-1929", "Calvin Coolidge", this.coolidge), new PresidentEntry("1929-1933", "Herbert Hoover", this.hoover), new PresidentEntry("1933-1937", "Franklin Roosevelt", this.fdr), new PresidentEntry("1937-1941", "Franklin Roosevelt", this.fdr), new PresidentEntry("1941-1945", "Franklin Roosevelt", this.fdr), new PresidentEntry("1945-1949", "Harry Truman", this.truman), new PresidentEntry("1949-1953", "Harry Truman", this.truman), new PresidentEntry("1953-1957", "Dwight Eisenhower", this.eisenhower), new PresidentEntry("1957-1961", "Dwight Eisenhower", this.eisenhower), new PresidentEntry("1961-1963", "John F. Kennedy", this.kennedy), new PresidentEntry("1963-1965", "Lyndon B. Johnson", this.lbj), new PresidentEntry("1965-1969", "Lyndon B. Johnson", this.lbj), new PresidentEntry("1969-1973", "Richard Nixon", this.nixon), new PresidentEntry("1973-1974", "Richard Nixon", this.nixon), new PresidentEntry("1974-1977", "Gerald Ford", this.ford), new PresidentEntry("1977-1981", "Jimmy Carter", this.carter), new PresidentEntry("1981-1985", "Ronald Reagan", this.reagan), new PresidentEntry("1985-1989", "Ronald Reagan", this.reagan), new PresidentEntry("1989-1993", "George H.W. Bush", this.h_w_bush), new PresidentEntry("1993-1997", "Bill Clinton", this.clinton), new PresidentEntry("1997-2001", "Bill Clinton", this.clinton), new PresidentEntry("2001-2005", "George W. Bush", this.w_bush), new PresidentEntry("2005-2009", "George W. Bush", this.w_bush), new PresidentEntry("2009-2013", "Barack Obama", this.obama), new PresidentEntry("2013-2017", "Barack Obama", this.obama), new PresidentEntry("2017-2021", "Donald Trump", this.trump), new PresidentEntry("2021-2025", "Joe Biden", this.biden), new PresidentEntry("2025-Present", "Donald Trump", this.trump)};
         int totalEntries = entries.length;
         int halfWay = (int)Math.ceil((double)totalEntries / 2.0);
@@ -96,11 +94,13 @@ public class App {
             }
             ++i;
         }
+        StringBuilder sb = new StringBuilder();
         i = 0;
         while (i < halfWay) {
-            System.out.printf("%-" + yearWidth + "s %-" + nameWidth + "s%-" + yearWidth + "s %-" + nameWidth + "s%n", displayGrid[i][0], displayGrid[i][1], displayGrid[i][2], displayGrid[i][3]);
+            sb.append(String.format("%-" + yearWidth + "s %-" + nameWidth + "s%-" + yearWidth + "s %-" + nameWidth + "s%n", displayGrid[i][0], displayGrid[i][1], displayGrid[i][2], displayGrid[i][3]));
             ++i;
         }
+        return sb.toString();
     }
 
     private boolean processAnswer(String answer) {
@@ -211,20 +211,20 @@ public class App {
         return true;
     }
 
-    private void displayWrongGuesses() {
+    private String displayWrongGuesses() {
         if (this.wrongGuesses.isEmpty()) {
-            return;
+            return "";
         }
-        System.out.print("Wrong guesses: ");
+        StringBuilder sb = new StringBuilder("Wrong guesses: ");
         int i = 0;
         while (i < this.wrongGuesses.size()) {
-            System.out.print(this.wrongGuesses.get(i));
+            sb.append(this.wrongGuesses.get(i));
             if (i < this.wrongGuesses.size() - 1) {
-                System.out.print(", ");
+                sb.append(", ");
             }
             ++i;
         }
-        System.out.println();
+        return sb.toString();
     }
 
     public static void clearConsole() {
